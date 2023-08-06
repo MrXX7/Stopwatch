b3ae57962eb6cbd98504f29dd2e38b9c916185bd
Oncu <oohancan@gmail.com>
2023-08-06 19:21:18 +0300

    feat: Add SwiftUI Stopwatch Experiment
    Added a simple SwiftUI-based stopwatch experiment to the project. The ContentView struct includes a stopwatch with start, stop, and reset functionality. The elapsed time is updated every 0.01 seconds using a Timer. The stopwatch can measure minutes, seconds, and milliseconds.
    
    This feature will allow users to time themselves while solving puzzles or other tasks. The elapsed time is displayed in a visually appealing format using SwiftUI's Views and styles.
    
    Changes Made:
    - Added ContentView struct with a stopwatch interface
    - Implemented start, stop, and reset functionality using @State variables
    - Used Timer to update the elapsed time every 0.01 seconds
    - Formatted and displayed the elapsed time in "mm:ss.ss" format
    
    Testing:
    Tested the stopwatch on different devices and orientations to ensure proper functionality and responsiveness.
    
    Known Issues:
    No known issues at this time. The stopwatch works as expected.
    
    Future Improvements:
    In future iterations, we could add lap time functionality and make the interface more customizable.

--- Stopwatch/ContentView.swift
+++ Stopwatch/ContentView.swift
@@ -8,14 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isRunning = false
+    @State private var elapsedTicks = 0
+    private let timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 46/255, green: 49/255, blue: 57/255)
+                .edgesIgnoringSafeArea(.all)
+
+            VStack {
+                Text("\(formatTime(elapsedTicks))")
+                    .font(.system(size: UIScreen.main.bounds.width / 5.5, weight: .thin, design: .monospaced))
+                    .foregroundColor(.white)
+                    .padding(.top, 50)
+
+                Spacer()
+
+                HStack(spacing: 20) {
+                    Button(action: startTimer) {
+                        Image(systemName: "play.fill")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .frame(width: 60, height: 60)
+                            .background(Color.green)
+                            .cornerRadius(30)
+                    }
+
+                    Button(action: stopTimer) {
+                        Image(systemName: "stop.fill")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .frame(width: 60, height: 60)
+                            .background(Color.red)
+                            .cornerRadius(30)
+                    }
+
+                    Button(action: resetTimer) {
+                        Image(systemName: "gobackward")
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .frame(width: 60, height: 60)
+                            .background(Color.blue)
+                            .cornerRadius(30)
+                    }
+                }
+                .padding(.bottom, 50)
+            }
         }
-        .padding()
+        .onReceive(timer) { _ in
+            if isRunning {
+                elapsedTicks += 1
+            }
+        }
+    }
+
+    private func startTimer() {
+        isRunning = true
+    }
+
+    private func stopTimer() {
+        isRunning = false
+    }
+
+    private func resetTimer() {
+        isRunning = false
+        elapsedTicks = 0
+    }
+
+    private func formatTime(_ ticks: Int) -> String {
+        let minutes = ticks / 6000
+        let seconds = (ticks / 100) % 60
+        let milliseconds = ticks % 100
+        return String(format: "%02d:%02d.%02d", minutes, seconds, milliseconds)
     }
 }
 
