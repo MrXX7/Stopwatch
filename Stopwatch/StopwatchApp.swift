4613545e3d360f8e5814164ec51ffbeaef203030
Oncu <oohancan@gmail.com>
2023-08-06 19:14:25 +0300

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Oncu Can on 6.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
